242d77fb0c03fd3c65bd15341a94f5bf93a65cc3
Nanaki404 <nanaki404@gmail.com>
2021-04-24 16:15:43 +0200

    ResourceStack add utilitiy functions

--- Assets/Scripts/ResourceStack.cs
+++ Assets/Scripts/ResourceStack.cs
@@ -45,7 +45,19 @@ public class ResourceStack : MonoBehaviour
         return (woodCount >= other.woodCount && foodCount >= other.foodCount && stoneCount >= other.stoneCount);
     }
 
-    public void Remove(ResourceStack other)
+    public float Divide(ResourceStack other)
+    {
+        float res = 10000000000;
+        if(other.woodCount>0) 
+            res = Mathf.Min(res, woodCount / other.woodCount);
+        if(other.foodCount>0) 
+            res = Mathf.Min(res, foodCount / other.foodCount);
+        if(other.stoneCount>0) 
+            res = Mathf.Min(res, stoneCount / other.stoneCount);
+        return res;
+    }
+
+    public void Substract(ResourceStack other)
     {
         woodCount -= other.woodCount;
         foodCount -= other.foodCount;
@@ -62,5 +74,51 @@ public class ResourceStack : MonoBehaviour
     public int GetSize()
     {
         return woodCount+foodCount+stoneCount;
+    }
+
+    // returns the remaining resources
+    public ResourceStack addWithinCapacity(ResourceStack other, int capacity)
+    {
+        int currentSize = GetSize();
+        if(currentSize >= capacity)
+            return other;
+        // here maybe change priority ?
+        if(currentSize + other.woodCount > capacity)
+        {
+            woodCount += capacity-currentSize;
+            other.woodCount -= capacity-currentSize;
+            return other;
+        }
+        else
+        {
+            woodCount += other.woodCount;
+            currentSize -= other.woodCount;
+            other.woodCount = 0;
+        }
+        if(currentSize + other.stoneCount > capacity)
+        {
+            stoneCount += capacity-currentSize;
+            other.stoneCount -= capacity-currentSize;
+            return other;
+        }
+        else
+        {
+            stoneCount += other.stoneCount;
+            currentSize -= other.stoneCount;
+            other.stoneCount = 0;
+        }
+        if(currentSize + other.foodCount > capacity)
+        {
+            foodCount += capacity-currentSize;
+            other.foodCount -= capacity-currentSize;
+            return other;
+        }
+        else
+        {
+            foodCount += other.foodCount;
+            currentSize -= other.foodCount;
+            other.foodCount = 0;
+        }
+        return other;
     }
 }
