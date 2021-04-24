f928d4f30ccdf22e9d0aaf7cf9d64ca9a7b09786
Astoeth <cyril.richard95@gmail.com>
2021-04-24 15:02:28 +0200

    Add Tile structure in map + worldObject base class

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -6,17 +6,19 @@ using UnityEngine.Tilemaps;
 public class Map : MonoBehaviour
 {
     
-    public int Width;
+    public int width;
     public int length;
     public Tilemap tilemap;
 
     public TileBase grassTile;
     public TileBase dirtTile;
 
+    public Tile[] tiles;
+
     void Awake()
     {
         tilemap = GetComponentInChildren<Tilemap>();
-        for (int x = 0; x < Width; x++)
+        for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < length; y++)
             {
@@ -36,4 +38,53 @@ public class Map : MonoBehaviour
     {
 
     }
+
+    void GenerateTiles()
+    {
+        tiles = new Tile[width * length];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                Tile tile = GetTile(x, y);
+                tile.isWater = false;
+                if (Random.value > 0.8)
+                {
+                    tile.texture = grassTile;
+                }
+                else
+                {
+                    tile.texture = dirtTile;
+                }
+            }
+        }
+    }
+
+    void GenerateElements()
+    {
+        // TODO add trees
+    }
+
+    void DrawTileMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                tilemap.SetTile(new Vector3Int(x, y, 0), GetTile(x, y).texture);
+            }
+        }
+    }
+
+    public Tile GetTile(int x, int y)
+    {
+        return tiles[y * width + x];
+    }
+}
+
+public class Tile
+{
+    public TileBase texture;
+    public bool isWater;
+    public WorldObject relatedObject;
 }
