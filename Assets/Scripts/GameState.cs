1cee26afcc7a891e0e0659d3943ba249a850219c
Franck <f13@laposte.net>
2021-04-25 12:13:49 +0200

    more icons

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,12 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SelectionState {
-    Empty,
-    BuildMode,
-    BuildingSelected,
-    UnitSelected
-}
 
 public class GameState : MonoBehaviour
 {
@@ -22,6 +16,13 @@ public class GameState : MonoBehaviour
 
     public static List<Vector2Int> neighboursVectorD = new List<Vector2Int> { new Vector2Int(1, 0),new Vector2Int(1,1),new Vector2Int(1,-1),new Vector2Int(0,1),new Vector2Int(0,-1),new Vector2Int(-1,0),new Vector2Int(-1,1),new Vector2Int(-1,-1) };
 
+    public enum SelectionState
+    {
+        Empty,
+        BuildMode,
+        BuildingSelected,
+        UnitSelected
+    }
     public SelectionState selection = SelectionState.Empty;
     public GameObject selected;
 
@@ -35,7 +36,7 @@ public class GameState : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ressources = new ResourceStack(20, 20, 20);
+        
     }
 
     // Update is called once per frame
