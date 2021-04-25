7f9ab7451eb4baf5e43cb9a2b7f1133456795de0
Fabien <fabien_herry@yahoo.fr>
2021-04-25 19:10:48 +0200

    bug

--- Assets/Scripts/Audio/AudioBank.cs
+++ Assets/Scripts/Audio/AudioBank.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class AudioBank : MonoBehaviour
 {
+    public enum AudioName
+    {
+        marche,
+        mine,
+        meurt,
+        fight,
+
+    }
     public AudioClip marche;
     public AudioClip mine;
     public AudioClip meurt;
     public AudioClip combat;
     public AudioClip buildingCrumble;
-    public AudioClip
+    public AudioClip robotMove;
+    public AudioClip robotFight;
+
+    public AudioClip GetSound()
+    {
+        return marche;
+    }
 
     // Start is called before the first frame update
     void Start()
